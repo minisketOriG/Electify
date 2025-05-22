78758dae94ff09b3870ea8fb8a59964f7756d965
minisketOriG <skirtm64+githubCoder@gmail.com>
2025-05-22 11:09:57 +0000

    working on variable edit

--- src/store/DataSlides/ContenderVarSlide.tsx
+++ src/store/DataSlides/ContenderVarSlide.tsx
@@ -24,6 +24,18 @@ export const contenderVarSlice = createSlice({
         removeContenderVariable: (state, action) => {
             const id  = action.payload;
             state.contenderVariables = state.contenderVariables.filter(variable => variable.id !== id);
+        },
+        editContenderVariable: (state, action) => {
+            const { id, variableName, requirementType, variableType } = action.payload;
+            
+            state.contenderVariables.map((variable) =>{
+                if(variable.id === id){
+                   return {...variable, name: variableName, requirement: requirementType, type: variableType} 
+                }else {    
+                return variable
+              }
+            });
+          
         }
     }
 })
